2f4a8d545c3ce5fcfee9b79913ae99919f22cb7f
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-12-26 00:12:59 +0530

    first commit and if else

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    // cout << "Hello Abhishek" << "\n" ;
+
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    cout << "Sum of a and b is " << c << "\n";
+
+    return 0;
+}
