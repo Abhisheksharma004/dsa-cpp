adbe320c2a594a65e9352c8dc95a6033af5172b0
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2025-01-04 01:22:53 +0530

    pattern 4

---
+++ pattern4.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+
+    // for(int i=0; i<n; i++){
+    //     char ch = 'A' + (i+1);
+    //     for (int j = 0; j<i+1; j++){
+    //         // cout << (i+1) << " ";
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for(int i=1; i<=n; i++){
+        char ch = 'A' + (i-1);
+        for (int j = 1; j<=i; j++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
