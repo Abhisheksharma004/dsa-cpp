e1cd61cea1410037e5c2523f96674586bfe6320a
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2025-01-05 01:12:59 +0530

    som pattern

---
+++ invertedTriangle.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cout << "Enter a Number : ";
+    cin >> n;
+
+    // for(int i=0; i<n; i++){
+
+    //     //spaces
+    //     for(int j=0; j<i; j++){
+    //         cout << " ";
+    //     }
+
+    //     //numbers
+    //     for(int j = 0; j<n-i; j++){
+    //         cout << (i+1);
+    //     }
+    //     cout << endl;
+    // }
+
+
+    for(int i=0; i<n; i++){
+
+        //spaces
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+
+        //numbers
+        for(int j = 0; j<n-i; j++){
+            cout << (i+1) << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
