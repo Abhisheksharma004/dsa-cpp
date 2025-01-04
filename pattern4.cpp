cba51bf14c7d9d6181bc28157f428542811d721d
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2025-01-05 00:40:19 +0530

    pattern 4,6,7

--- pattern4.cpp
+++ pattern4.cpp
@@ -4,7 +4,7 @@ int main(){
     // int n;
     // cout << "Enter a number: ";
     // cin >> n;
-
+ 
     // for(int i=0; i<n; i++){
     //     char ch = 'A' + (i+1);
     //     for (int j = 0; j<i+1; j++){
